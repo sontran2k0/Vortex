085f5304b573fd4e2274b285670b78583ed35d22
sontran2k0 <trtr.shan@gmail.com>
2026-01-29 22:29:56 +0700

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,57 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+  const env = loadEnv(mode, '.', '')
+
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0'
+    },
+
+    plugins: [
+      react(),
+
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico'],
+        manifest: {
+          name: 'Vortex Cards',
+          short_name: 'Vortex',
+          description: 'Vortex Cards',
+          theme_color: '#0f124088',
+          background_color: '#09082eac',
+          display: 'standalone',
+          start_url: '/',
+          icons: [
+            {
+              src: '/logo.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/logo.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
         }
+      })
+    ],
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.')
       }
-    };
-});
+    }
+  }
+})
+
